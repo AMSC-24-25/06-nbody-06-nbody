6a1dc08a05fa49855e4742354af63b46a00fc973
AndreaBarletta <10665046@polimi.it>
2024-12-02 16:20:53 +0100

    Added test file for the body class

---
+++ tests/body_test/body_test.cpp
@@ -0,0 +1,13 @@
+#include <Body.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    Body2d b1(1.0,{1.0,2.0},{2.0,-3.0});
+    Body2d b2(4.0,{3.0,-1.0},{6.0,4.0});
+
+    cout << b1 << b2 << endl;
+    return 0;
+}
