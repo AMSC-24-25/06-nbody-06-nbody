5eb9a77677911421258ed5f181a7f463b28064c1
AndreaBarletta <10665046@polimi.it>
2024-12-02 17:20:39 +0100

    Added method to update position, velocity and acceleration (with the relative test)

--- tests/body_test/body_test.cpp
+++ tests/body_test/body_test.cpp
@@ -9,5 +9,11 @@ int main(int argc, char **argv)
     Body2d b2(4.0,{3.0,-1.0},{6.0,4.0});
 
     cout << b1 << b2 << endl;
+
+    Vector2d delta({10.0,20.0});
+    b1.updatePosition(2*delta);
+    b2.updateVelocity(3*delta);
+
+    cout << b1 << b2 << endl;
     return 0;
 }
