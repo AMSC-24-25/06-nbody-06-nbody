3dbbcab118669f429a992f4531c70b6d166c4d78
AndreaBarletta <10665046@polimi.it>
2024-12-03 18:18:19 +0100

    3 body test case

--- tests/solver_test/solver_test.cpp
+++ tests/solver_test/solver_test.cpp
@@ -5,32 +5,30 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    //1D SOLVER
-    const double deltaT = 100;
-    const double numBodies = 2;
-    const double T = 4e5;
-    const unsigned int outputFreq = 5;
+    // 2d 3 bodies (known configuration)
+    const double deltaT = 1e-2;
+    const double numBodies = 3;
+    const double T = 1;
+    const unsigned int outputFreq = 1;
 
-    NbodySolver solver(numBodies,deltaT);
+    NbodySolver solver(numBodies, deltaT);
 
-    //Add bodies
-    Vector<double,2> pos1({0.0,0.0});
-    Vector<double,2> vel1({0.0,0.0});
-    solver.addBody(5.970e24,pos1,vel1);
+    // Add bodies
+    solver.addBody(1.0, Vector2d({-2.0, 0.0}), Vector2d({0.3672, 0.5635}));
+    solver.addBody(1.0, Vector2d({0.0, 0.0}), Vector2d({-0.3672, -0.5635}));
+    solver.addBody(1.0, Vector2d({2.0, 0.0}), Vector2d({0.3672, 0.5635}));
 
-    Vector<double,2> pos2({1e8,0.0});
-    Vector<double,2> vel2({0.0,1.996e3});
-    solver.addBody(1,pos2,vel2);
-
-    //Proceed with the simulation
+    // Proceed with the simulation
     unsigned int timestepCounter = 0;
-    for(double t=0;t<T;t+=deltaT){
+    for (double t = 0; t < T; t += deltaT)
+    {
         solver.step();
-        if(timestepCounter%outputFreq==0){
+        if (timestepCounter % outputFreq == 0)
+        {
             solver.output();
         }
         timestepCounter++;
     }
-    
+
     return 0;
 }
