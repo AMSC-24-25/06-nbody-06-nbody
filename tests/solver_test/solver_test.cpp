8ed63d2e73c80c33aebfe0e32292badc6c5896f5
AndreaBarletta <10665046@polimi.it>
2024-12-04 10:10:02 +0100

    New test configuration

--- tests/solver_test/solver_test.cpp
+++ tests/solver_test/solver_test.cpp
@@ -6,17 +6,18 @@ using namespace std;
 int main(int argc, char **argv)
 {
     // 2d 3 bodies I.A. i.c. 1 from https://numericaltank.sjtu.edu.cn/three-body/three-body-movies.htm
-    const double deltaT = 1e-5;
+    const double deltaT = 1e-4;
     const double numBodies = 3;
-    const double T = 10;
+    const double T = 5;
     const unsigned int outputFreq = 1000;
 
     NbodySolver solver(numBodies, deltaT);
 
+
     // Add bodies
-    solver.addBody(1.0, Vector2d({-1.0, 0.0}), Vector2d({0.3471168881, 0.5327249454}));
-    solver.addBody(1.0, Vector2d({1.0, 0.0}), Vector2d({0.3471168881, 0.5327249454}));
-    solver.addBody(1.0, Vector2d({0.0, 0.0}), Vector2d({-2*0.3471168881, -2*0.5327249454}));
+    solver.addBody(1.0, Vector2d({0.6661637520772179,-0.081921852656887}), Vector2d({0.84120297540307,0.029746212757039}));
+    solver.addBody(1.0, Vector2d({-0.025192663684493022,0.45444857588251897}), Vector2d({0.142642469612081,-0.492315648524683}));
+    solver.addBody(1.0, Vector2d({-0.10301329374224,-0.765806200083609}), Vector2d({-0.98384544501151,0.462569435774018}));
 
     // Proceed with the simulation
     unsigned int timestepCounter = 0;
