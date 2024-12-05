234b7f76c6c95c83b37f579ab3f982af968a96ff
AndreaBarletta <10665046@polimi.it>
2024-12-05 12:37:50 +0100

    Fixed iotest based on the new file format

--- tests/io_test/io_test.cpp
+++ tests/io_test/io_test.cpp
@@ -9,12 +9,20 @@
 void testReadBodies()
 {
     std::ofstream tempInput("testInput.txt");
-    tempInput << "1.0 1.0 2.0 2.0 -3.0\n"; // Body2d 1
-    tempInput << "4.0 3.0 -1.0 6.0 4.0\n"; // Body2d 2
+    // Number of bodies
+    tempInput << "2" << std::endl;
+    // First body
+    tempInput << "1.0" << std::endl;      // mass
+    tempInput << "1.0 2.0" << std::endl;  // position
+    tempInput << "2.0 -3.0" << std::endl; // velocity
+    // Second body
+    tempInput << "4.0" << std::endl;      // mass
+    tempInput << "3.0 -1.0" << std::endl; // position
+    tempInput << "6.0 4.0" << std::endl;  // velocity
     tempInput.close();
 
     // Read bodies using IO class
-    std::vector<Body2d> bodies = IO::readBodiesFromFile("testInput.txt");
+    std::vector<Body2d> bodies = IO<double, 2>::readBodiesFromFile("testInput.txt");
 
     // Detailed checks
     assert(bodies.size() == 2);
@@ -34,7 +42,7 @@ void testReadBodies()
     assert(bodies[1].getVelocity()[1] == 4.0);
 
     std::cout << "Test read bodies works." << std::endl;
-    std::remove("testInput.txt");
+    // std::remove("testInput.txt");
 }
 
 void testWriteBodies()
@@ -43,7 +51,7 @@ void testWriteBodies()
         Body2d(1.0, {1.0, 2.0}, {2.0, -3.0}),
         Body2d(4.0, {3.0, -1.0}, {6.0, 4.0})};
 
-    IO::writeBodiesToFile("testOutput.txt", bodies);
+    IO<double, 2>::writeBodiesToFile("testOutput.txt", bodies);
 
     // More robust file reading
     std::ifstream tempOutput("testOutput.txt");
@@ -59,11 +67,16 @@ void testWriteBodies()
     }
 
     // Check number of lines
-    assert(lines.size() == 2);
+    assert(lines.size() == 7);
 
     // Direct string comparison
-    assert(lines[0] == "1 1 2 2 -3");
-    assert(lines[1] == "4 3 -1 6 4");
+    assert(lines[0] == "2");
+    assert(lines[1] == "1");
+    assert(lines[2] == "1 2");
+    assert(lines[3] == "2 -3");
+    assert(lines[4] == "4");
+    assert(lines[5] == "3 -1");
+    assert(lines[6] == "6 4");
 
     std::cout << "Test write bodies works." << std::endl;
 
