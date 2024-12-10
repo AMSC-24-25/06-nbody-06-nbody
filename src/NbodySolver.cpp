5215d5e421df50ee8911c36bfcca881c7a7c4044
AndreaBarletta <10665046@polimi.it>
2024-12-10 20:47:19 +0100

    Parallelized (allegedly) using openmp

--- src/NbodySolver.cpp
+++ src/NbodySolver.cpp
@@ -1,5 +1,5 @@
 #include <NbodySolver.hpp>
-
+#include <omp.h>
 
 void NbodySolver::loadBodies(const std::string &bodies_file_name)
 {
@@ -9,28 +9,36 @@ void NbodySolver::loadBodies(const std::string &bodies_file_name)
 
 void NbodySolver::step(const Real &deltaT)
 {
-    std::vector<Vector<Real, dim>> forces(numBodies);
-    // Compute forces
+    std::vector<std::vector<Vector<Real, dim>>> forces(numBodies, std::vector<Vector<Real, dim>>(numBodies));
+    // Compute (local) forces
     {
-        // TODO: THIS IS THE PART THAT CAN BE PARALLELIZED
+        #pragma omp parallel for
         for (int q = 0; q < bodies.size(); q++)
         {
             for (int p = q + 1; p < bodies.size(); p++)
             {
-                forces[q] += computeForce(bodies[q], bodies[p]);
-                forces[p] -= computeForce(bodies[q], bodies[p]);
+                Vector<Real, dim> qpForce = computeForce(bodies[q], bodies[p]);
+                forces[omp_get_thread_num()][q] += qpForce;
+                forces[omp_get_thread_num()][p] -= qpForce;
             }
         }
     }
 
     // Update bodies properties (position and velocity are updated using the old value)
     {
+        #pragma omp parallel for
         for (int q = 0; q < bodies.size(); q++)
         {
+            Compute total force from local forces
+            Vector<Real, dim> totForce;
+            for (int i = 0; i < bodies.size(); i++)
+            {
+                totForce += forces[i][q];
+            }
             // Leapfrog KDK integrator
             bodies[q].updateVelocity(0.5 * bodies[q].getAcceleration() * deltaT);
             bodies[q].updatePosition(bodies[q].getVelocity() * deltaT);
-            bodies[q].setAcceleration(forces[q] / bodies[q].getMass());
+            bodies[q].setAcceleration(totForce / bodies[q].getMass());
             bodies[q].updateVelocity(0.5 * bodies[q].getAcceleration() * deltaT);
         }
     }
@@ -38,10 +46,10 @@ void NbodySolver::step(const Real &deltaT)
 
 void NbodySolver::outputData(std::ofstream &output_file) const
 {
-    IO<Real,dim>::writeBodiesInfo(output_file,bodies);
+    IO<Real, dim>::writeBodiesInfo(output_file, bodies);
 }
 
 void NbodySolver::outputTimestep(std::ofstream &output_file) const
 {
-    IO<Real,dim>::writeSimulationData(output_file,bodies);
+    IO<Real, dim>::writeSimulationData(output_file, bodies);
 }
