53a0eefff520dd2b119c32c74dbafa22225cac90
supertramp277 <18621056089@163.com>
2024-12-16 19:40:08 +0000

    Extract Energy Function

--- include/NBodyBHSolver.hpp
+++ include/NBodyBHSolver.hpp
@@ -20,9 +20,36 @@ public:
     NBodyBHSolver(const Quad<T> &universe, T timeStep = 0.1)
         : universe(universe), timeStep(timeStep) {}
 
+    const std::vector<Body<T, 2>> &getBodies() const
+    {
+        return bodies;
+    }
+
     void addBody(const Body<T, 2> &body)
     {
         bodies.push_back(body);
+    }
+
+    void calculateEnergy()
+    {
+        // Calculate energy of each body
+        for (Body<T, 2> &body : bodies)
+        {
+            T newKineticEnergy = 0.5 * body.getMass() * body.getVelocity().norm() * body.getVelocity().norm();
+            T newPotentialEnergy = 0.0;
+            for (Body<T, 2> &other : bodies)
+            {
+                if (&body != &other)
+                {
+                    Vector<T, 2> r = body.getPosition() - other.getPosition();
+                    newPotentialEnergy -= body.getMass() * other.getMass() / r.norm();
+                }
+            }
+            // Total energy is the sum of kinetic and potential energy, divide by 2 to avoid double counting
+            // This should be right because when we add them together, we just get original potential energy
+            T newEnergy = newKineticEnergy + newPotentialEnergy / 2.0;
+            body.setEnergy(newEnergy);
+        }
     }
 
     void simulateOneStep()
@@ -56,10 +83,6 @@ public:
         {
             Vector<T, 2> deltaPos = body.getVelocity() * timeStep;
             body.updatePosition(deltaPos);
-            // Todo: Handle boundary conditions?
-            // if (!universe.contains(body.getPosition()))
-            // {
-            //   Periodic boundaries or elastic collision?
         }
 
         // Rebuild tree and recalculate forces at new positions
@@ -84,39 +107,6 @@ public:
         {
             Vector<T, 2> deltaVel = body.getAcceleration() * timeStep * 0.5;
             body.updateVelocity(deltaVel);
-        }
-
-        // Calculate energy of each body
-        for (Body<T, 2> &body : bodies)
-        {
-            T newKineticEnergy = 0.5 * body.getMass() * body.getVelocity().norm() * body.getVelocity().norm();
-            T newPotentialEnergy = 0.0;
-            for (Body<T, 2> &other : bodies)
-            {
-                if (&body != &other)
-                {
-                    Vector<T, 2> r = body.getPosition() - other.getPosition();
-                    newPotentialEnergy -= body.getMass() * other.getMass() / r.norm();
-                }
-            }
-            // Total energy is the sum of kinetic and potential energy, divide by 2 to avoid double counting
-            // This should be right because when we add them together, we just get original potential energy
-            T newEnergy = newKineticEnergy + newPotentialEnergy / 2.0; 
-            body.setEnergy(newEnergy);
-        }
-    }
-
-    const std::vector<Body<T, 2>> &getBodies() const
-    {
-        return bodies;
-    }
-
-    void output()
-    {
-        for (size_t i = 0; i < bodies.size(); i++)
-        {
-            std::cout << bodies[i].getPosition() << std::endl
-                      << bodies[i].getAcceleration() << std::endl;
         }
     }
 };
